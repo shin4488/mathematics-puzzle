e7a323a5599c0ace0e13ff863e0af7581bafc750
shin4488 <efshinya48@gmail.com>
2022-08-04 16:01:09 +0900

    add: insertion sort algorithm

---
+++ other/insertion_sort.rb
@@ -0,0 +1,19 @@
+require './other/common'
+
+execute_insertion_sort = lambda do |numbers|
+    numbers_size = numbers.size
+    (1...(numbers_size)).each do |i|
+        compared_index = i - 1
+        while compared_index >= 0 && numbers[i] < numbers[compared_index] do
+            compared_index -= 1
+        end
+
+        target_number = numbers[i]
+        numbers.delete_at(i)
+        numbers.insert(compared_index + 1, target_number)
+    end
+
+    return numbers
+end
+
+execute(&execute_insertion_sort)
