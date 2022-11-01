a1d0dfa17a7185f8072e3ab97f02899bdb67762f
shin4488 <efshinya48@gmail.com>
2022-11-02 00:38:59 +0900

    add: bit all search algorithm

---
+++ other/indivisual/bitAllSearch.js
@@ -0,0 +1,21 @@
+// aの要素をいくつか選択して、合計がwになる組み合わせ（部分和）
+w = 10;
+a = [1, 2, 3, 4, 5];
+
+n = a.length;
+for (let i = 0; i < 1 << n; i++) {
+  const targets = [];
+  for (let j = 0; j < n; j++) {
+    if ((i & (1 << j)) === 0) {
+      continue;
+    }
+
+    targets.push(a[j]);
+  }
+
+  const sum = targets.reduce((sum, element) => sum + element, 0);
+  if (sum === w) {
+    const resultNumberText = targets.join('');
+    console.log(resultNumberText);
+  }
+}
